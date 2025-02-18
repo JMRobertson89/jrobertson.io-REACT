7b1ddf88a8f77d2181f8c3baa4857cc16a96cbec
JMRobertson89 <joshrobertson1618@gmail.com>
2025-02-17 21:38:52 -0500

    completed routing

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import Sidebar from './Sidebar';
+
+function Header() {
+    return (
+        <header>
+            <h1>Header</h1>
+            <Sidebar />
+        </header>
+    );
+};
+
+export default Header;
--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -1,40 +1,22 @@
-import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import Footer from './Footer';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Projects from '../pages/Projects';
+import Contact from '../pages/Contact';
+import Blog from '../pages/Blog';
 
 export default function PortfolioContainer() {
-	const [currentPage, setCurrentPage] = useState('Home');
-
-	const renderPage = () => {
-        if (currentPage === 'Home') {
-            return <Home />;
-        }
-		if (currentPage === 'About') {
-			return <About />;
-		}
-		if (currentPage === 'Projects') {
-			return <Projects />;
-		}
-        if (currentPage === 'Blog') {
-			return <Blog />;
-		}
-		if (currentPage === 'Contact') {
-			return <Contact />;
-		}
-	};
-
-	const handlePageChange = (page) => setCurrentPage(page);
-
 	return (
-		<>
-			<Header handlePageChange={handlePageChange} />
-
-			{renderPage()}
-
-			<Footer />
-		</>
+		<Router>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/projects" element={<Projects />} />
+				<Route path="/blog" element={<Blog />} />
+				<Route path="/contact" element={<Contact />} />
+			</Routes>
+		</Router>
 	);
 }
