bbeef1c1311fcf50997fa9c40b78f67eeddf8a04
JMRobertson89 <joshrobertson1618@gmail.com>
2025-02-17 23:05:04 -0500

    started sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import Blog from './pages/Blog';
 import PageNotFound from './pages/PageNotFound';
+import './App.css';
 
 
 function App() {
 	return (
 		<Router>
-			<Header />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/projects" element={<Projects />} />
-				<Route path="/blog" element={<Blog />} />
-				<Route path="/contact" element={<Contact />} />
-				{/* 404 route */}
-				<Route path="*" element={<PageNotFound />} />
-			</Routes>
+			<div className="app-container">
+				<Sidebar />
+				<div className="content-container">
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/about" element={<About />} />
+						<Route path="/projects" element={<Projects />} />
+						<Route path="/blog" element={<Blog />} />
+						<Route path="/contact" element={<Contact />} />
+						{/* 404 route */}
+						<Route path="*" element={<PageNotFound />} />
+					</Routes>
+				</div>
+			</div>
 		</Router>
 	);
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,45 @@
+import { Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Sidebar.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 function Sidebar() {
 	return (
-		<div>
-			<h1>Sidebar</h1>
+		<div className="sidebar d-flex flex-column p-3">
+			<h2 className="logo">JR.</h2>
+			<Nav>
+				<Nav.Link as={Link} to="/" className="nav-item">
+					<i className="bi bi-house-door"></i>
+				</Nav.Link>
+				<Nav.Link as={Link} to="/about" className="nav-item">
+					<i className="bi bi-person"></i>
+				</Nav.Link>
+				<Nav.Link as={Link} to="/projects" className="nav-item">
+					<i className="bi bi-file-code-fill"></i>
+				</Nav.Link>
+				<Nav.Link as={Link} to="/blog" className="nav-item">
+					<i className="bi bi-book"></i>
+				</Nav.Link>
+				<Nav.Link as={Link} to="/contact" className="nav-item">
+					<i className="bi bi-envelope"></i>
+				</Nav.Link>
+				<Nav.Link
+					href="https://github.com/JMRobertson89"
+					target="_blank"
+					className="nav-item"
+				>
+					<i className="bi bi-github"></i>
+				</Nav.Link>
+				<Nav.Link
+					href="https://www.linkedin.com/in/jrobertson-io/"
+					target="_blank"
+					className="nav-item"
+				>
+					<i className="bi bi-linkedin"></i>
+				</Nav.Link>
+			</Nav>
 		</div>
 	);
 }
+
 export default Sidebar;
