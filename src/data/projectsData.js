ecd7e73319290dadf8dffd8309fa8ed3fec849ae
JMRobertson89 <joshrobertson1618@gmail.com>
2025-02-22 14:59:38 -0500

    blog page and blogs posted

---
+++ src/data/projectsData.js
@@ -0,0 +1,32 @@
+const projectsData = [
+    {
+        title: "Social Network API",
+        description: "Social network API created using Node.js, Express.js, using MongoDB",
+        image: "",
+        codeLink: "https://github.com/JMRobertson89/Social-Network-API",
+        appLink: "https://www.youtube.com/watch?v=7nFSZ9iiGTU",
+    },
+    {
+        title: "Curated Crates",
+        description: "A subscription-based e-commerce website built collaboratively with React, Node.js, Material-UI and MongoDB",
+        image: "",
+        codeLink: "https://github.com/JMRobertson89/Curated-Crates",
+        appLink: "https://curated-crates.onrender.com/",
+    },
+    {
+        title: "Random Ideas",
+        description: "A full-Stack application for sharing random ideas. Built with Express, Webpack, and MongoDB",
+        image: "",
+        codeLink: "https://github.com/JMRobertson89/Random-Ideas",
+        appLink: "https://random-ideas-api-zdkv.onrender.com/",
+    },
+    {
+        title: "Employee Tracker",
+        description: "Command-line application to manage an employee database, using Node.js, Inquirer, and PostgreSQL",
+        image: "",
+        codeLink: "https://github.com/JMRobertson89/Employee-Tracker",
+        appLink: "https://www.youtube.com/watch?v=Wh7yypVWQlQ",
+    }
+]
+
+export default projectsData;
