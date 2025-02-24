978ede62e7010a52654fa1cd2108e698e714e5d8
JMRobertson89 <joshrobertson1618@gmail.com>
2025-02-24 13:04:09 -0500

    finished projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,11 +5,36 @@ function Projects() {
 	return (
 		<div className="projects-container">
 			<h1>Projects</h1>
-			<div className='projects'>
+			<div className="projects">
 				{projectsData.map((project) => (
-					<div className='project-card' key={project.id}>
-						<div className='project-image'>
+					<div className="project-card" key={project.id}>
+						<div className="project-info">
+							<h4>{project.title}</h4>
+							{/* <p>{project.description}</p> */}
+						</div>
+						<div className="project-image-container">
 							<img src={project.image} alt={project.title} />
+							<div className="project-overlay">
+								<div className='project-description'>
+									<p>{project.description}</p>
+								</div>
+								<div className="project-buttons">
+									<a
+										href={project.codeLink}
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<button>View Code</button>
+									</a>
+									<a
+										href={project.appLink}
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<button>View App</button>
+									</a>
+								</div>
+							</div>
 						</div>
 					</div>
 				))}
